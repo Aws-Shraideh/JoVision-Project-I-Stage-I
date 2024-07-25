80da5143a3b2f7327f61e356eab7aed7ec8b6071
Aws-Shraideh <amalshraiedeh21@cit.just.edu.jo>
2024-07-25 15:26:19 +0300

    Your commit message

--- App.tsx
+++ App.tsx
@@ -1,64 +1,14 @@
 import React from 'react';
-import Task16 from './Tasks/Task16';
-import Task17 from './Tasks/Task17';
-import Task18 from './Tasks/Task18';
-import Task19 from './Tasks/Task19';
-import Task20 from './Tasks/Task20';
-import Task21 from './Tasks/Task21';
-import Task22 from './Tasks/Task22';
-import Task23 from './Tasks/Task23';
-import Task24 from './Tasks/Task24';
-import Task25 from './Tasks/Task25';
-import Task26 from './Tasks/Task26';
-import Task27 from './Tasks/Task27';
-import Task28 from './Tasks/Task28';
-import Task29 from './Tasks/Task29';
-import Task30 from './Tasks/Task30';
-import Task31 from './Tasks/Task31';
-import Task32 from './Tasks/Task32';
-import Task33 from './Tasks/Task33';
-import Task34 from './Tasks/Task34';
-import Task35 from './Tasks/Task35';
-import Task36 from './Tasks/Task36';
-import Task37 from './Tasks/Task37';
-import Task38 from './Tasks/Task38';
-import Task39 from './Tasks/Task39';
-import Task40 from './Tasks/Task40';
-import Task41 from './Tasks/Task41';
-import Task42 from './Tasks/Task42';
+import Project1Stage1 from './Tasks/Project1Stage1';
+
 
 
 
 export default function App()
 {
   return (
-      //<Task16></Task16>
-      //<Task17></Task17>
-      //<Task18></Task18>
-      //<Task19></Task19>
-      //<Task20></Task20>
-      //<Task21></Task21>
-      //<Task22></Task22>
-      //<Task23></Task23>
-      //<Task24></Task24>
-      //<Task25></Task25>
-      //<Task26></Task26>
-      //<Task27></Task27>
-      //<Task28></Task28>
-      //<Task29></Task29>
-      //<Task30></Task30>
-      //<Task31></Task31>
-      //<Task32></Task32>
-      //<Task33></Task33>
-      //<Task34></Task34>
-      //<Task35></Task35>
-      //<Task36></Task36>
-      //<Task37></Task37>
-      //<Task38></Task38>
-      //<Task39></Task39>
-      //<Task40></Task40>
-      //<Task41></Task41>
-      <Task42></Task42>
+
+      <Project1Stage1></Project1Stage1>
 
 
   );
